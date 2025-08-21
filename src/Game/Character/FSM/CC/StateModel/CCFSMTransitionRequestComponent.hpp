c6e096cef4277d4acd3926c1e0eb605959eac8f3
Yamato <megabroccolin@gmail.com>
2025-08-21 23:21:57 +0900

    feat(cc-fsm/state-model): CCFSMTransitionRequestComponent を追加

---
+++ src/Game/Character/FSM/CC/StateModel/CCFSMTransitionRequestComponent.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Game/Character/FSM/CC/CCStateTags.hpp"
+
+#include <typeindex>
+#include <vector>
+#include <algorithm>
+namespace Game::Character::FSM::CC::StateModel
+{
+
+	struct CCFSMTransitionRequestComponent
+	{
+		struct CCFSMTransitionRequest
+		{
+			std::type_index requestedTo = StateTag::NONE;
+			uint32_t priority = 0;
+		};
+
+		struct MovementFSMTransitionRequestComponent
+		{
+			std::vector<CCFSMTransitionRequest> requests;
+
+			bool hasExactRequest(const std::type_index& target, uint32_t priority) const
+			{
+				// ‚¢‚¸‚ê‚©
+				return std::any_of(requests.begin(), requests.end(),
+					[&](const CCFSMTransitionRequest& req)
+					{
+						return req.requestedTo == target && req.priority == priority;
+					});
+			};
+		};
+	};
+}
