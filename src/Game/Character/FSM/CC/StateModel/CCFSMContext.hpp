97362b46f0b22d8c57b4e1eec116a042b8c210a1
Yamato <megabroccolin@gmail.com>
2025-08-23 23:16:58 +0900

    fix(cc-fsm/CCFSMContext): CCFSMContext構造体のdominantTag変数の初期化を指定

--- src/Game/Character/FSM/CC/StateModel/CCFSMContext.hpp
+++ src/Game/Character/FSM/CC/StateModel/CCFSMContext.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
-
+#include "Game/Character/FSM/CC/CCStateTags.hpp"
 
 #include <typeindex>
 
 namespace Game::Character::FSM::CC::StateModel
 {
+
 	struct CCFSMContext
 	{
-		std::type_index dominantTag;
+		std::type_index dominantTag = StateTag::NONE;
 		bool hasActiveCC = false;
 	};
 }
