56e26eddf07d553846442e01a60d90d4e659dee4
Yamato <megabroccolin@gmail.com>
2025-09-16 16:30:43 +0900

    feat(skill-comp/hitmemo): src/Game/Combat/Skill/Component/HitboxHitMemoComponent.hppを追加
    - HitEventを一度の対象(target)に対して一度だけ発行するようにするため
    - SkillHitboxEntityに衝突済み対象(target)のEntityIDを記録するためのデータを追加

---
+++ src/Game/Combat/HitEvent/API/GenerateHitEvent.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "Engine/ECS/Entity.h"
+#include "Engine/ECS/EntityManager.h"
+
+
+#include "Game/Combat/HitEvent/Data/HitEvent.hpp"
+#include "Game/Combat/HitEvent/Database/HitEventDatabase.hpp"
+
+#include "Common/EngineNamespaceDecl.h"
+
+#include <cstdint>
+
+namespace Game::Combat::HitEvent::API
+{
+	inline void GenerateHitEvent(eNsECS::EntityMgr& ecs,
+		const eNsECS::Entity skill,
+		const eNsECS::Entity target,
+		const uint32_t skillId,
+		const float now,
+		Game::Combat::HitEvent::Database::HitEventDatabase& db
+	)
+	{
+		Game::Combat::HitEvent::Data::HitEvent ev{};
+		ev.skill = skill;
+		ev.target = target;
+		ev.skillId = skillId;
+		ev.SpawnTime = now;
+
+		db.push(std::move(ev));
+	}
+}
